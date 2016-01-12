1a68b1a1048d963fc0f7965682c4112e0fdc2966
Jackson Cafazzo <doctorjuno@gmail.com>
2016-01-11 17:44:46 -0800

    change header color, add shadow and add pointer on hover over

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
   $(".carousel-control").click(function() {
     $(".hiddn").hide();
   });
+
 });
 
 //Great idea putting your flashcards in a carousel! Saved a lot of time not having to add invidual jQuery strings for each item.
